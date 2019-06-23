fff1bf2c50d8b66e6fb8f6cb7d5a737dd0f4de08
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 17:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@
 //   "4".
 // 4. Create a variable called `max` that will return "20" as the highest number in
 //   the set.
+
+function multiply(num1, num2) {
+ const num1 = 62 
+const num2 = 1 
+  
+  num1 * num2 
+  
+}
