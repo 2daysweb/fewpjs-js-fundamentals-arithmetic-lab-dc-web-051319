a9f99b1cf61ab92c6d705b12458ab5be4fca9647
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 17:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// ## Instructions
 
-// Write your code here
+
+// 2. Create a variable called `random` that will generate a random integer greater
+//   than 0.
+// 3. Create a variable called `mod` set to an equation whose remainder is equal to
+//   "4".
+// 4. Create a variable called `max` that will return "20" as the highest number in
+//   the set.
+
+
+
+
+const num1 = 62 
+const num2 = 1 
+const multiply = num1*num2 
+
+
+let random = Math.random() + .00001 
+let mod = 19%5 
+let max = Math.max(20, 10, 9)
+
+
+
+
+ 
